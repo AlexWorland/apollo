316c0ea2b2e7fd67d788d6b369d76e7b73534e0c
AlexWorland <alexworland@gmail.com>
2025-11-22 18:56:22 -0800

    Initialize host_max_bitrate_kbps to 0 for safety

--- src/rtsp.h
+++ src/rtsp.h
@@ -58,7 +58,7 @@ namespace rtsp_stream {
     std::list<crypto::command_entry_t> client_do_cmds;
     std::list<crypto::command_entry_t> client_undo_cmds;
 
-    int host_max_bitrate_kbps;  // Host's maximum bitrate limit (0 if unlimited)
+    int host_max_bitrate_kbps = 0;  // Host's maximum bitrate limit (0 if unlimited)
 
   #ifdef _WIN32
     GUID display_guid{};
